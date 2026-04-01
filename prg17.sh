afb6e1e57d57513c33b09a3c9ccb29693bd6c402
Ranthana k <ranthana2003@gmail.com>
2026-04-01 06:42:31 +0530

    Update prg17.sh

--- prg17.sh
+++ prg17.sh
@@ -1,19 +1,9 @@
 
-read -p "enter file name: " filename
+read -p "Enter file name: " filename
 
-if [ -f "$filename" ]; then
-    line_c=0
-    word_c=0
-    while IFS= read -r line
-    do
-        line_c=$(($line_c + 1))
-        for word in $line
-        do
-            word_c=$((word_c + 1))
-        done
-    done < "$filename"
-    echo "number of lines: $line_c"
-    echo "number of words: $word_c"
+if [ -f "$filename" ]
+then
+  wc -l -w "$filename"
 else
-    echo "file not found"
+  echo "file not found"
 fi
