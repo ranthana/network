0664b3052186c918a17c0d7d2db2affa4ee12937
Ranthana k <ranthana2003@gmail.com>
2026-04-01 05:40:29 +0530

    Update prg9.sh

--- prg9.sh
+++ prg9.sh
@@ -1,17 +1,16 @@
 
 #!/bin/bash
 
-read -p "Enter first number: " a
-read -p "Enter second number: " b
-read -p "Enter third number: " c
-read -p "Enter fourth number: " d
+read -p "Enter numbers: " a b c d
 
-if [ "$a" -lt "$b" ] && [ "$a" -lt "$c" ] && [ "$a" -lt "$d" ]; then
-    echo "$a is the smallest number"
-elif [ "$b" -lt "$a" ] && [ "$b" -lt "$c" ] && [ "$b" -lt "$d" ]; then
-    echo "$b is the smallest number"
-elif [ "$c" -lt "$a" ] && [ "$c" -lt "$b" ] && [ "$c" -lt "$d" ]; then
-    echo "$c is the smallest number"
-else
-    echo "$d is the smallest number"
-fi
+smallest=$a
+
+for num in $b $c $d
+do
+  if [ $num -lt $smallest ]
+  then
+    smallest=$num
+  fi
+done
+
+echo "$smallest is the smallest number"
